42503152130496331d119e1495cfa461d29c7de0
DDemmer1 <Dennis.Demmer@web.de>
2020-11-09 19:42:55 +0100

    initial commit

---
+++ src/mypackage/Application.java
@@ -0,0 +1,18 @@
+package mypackage;
+
+class Application {
+
+	//Das ist ein Kommentar
+	
+	//"Main Methode" Startpunkt der Anwendung
+	//Shortcut Autovervollständigung: main eingeben -> STRG+LEERTASTE
+	public static void main(String args[]) {
+		
+		//Befehl um Zeichenketten in der Konsole ausgeben zu lassen
+		System.out.println("Hello World");
+		
+		
+	}
+	
+	
+}
